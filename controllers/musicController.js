9bf5fe2433c03d98e3279788dd8658cf3e4d1202
AYAZ MAHMOOD <hmayaz528@gmail.com>
2025-02-01 16:54:20 +0500

    fixed file paths issue

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,24 +1,24 @@
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
-const uploadMusic = (req, res) => {
-  const page = parseInt(req.params.page);
+// const uploadMusic = (req, res) => {
+//   const page = parseInt(req.params.page);
 
-  if (!req.file) return res.status(400).send("No music file uploaded.");
+//   if (!req.file) return res.status(400).send("No music file uploaded.");
 
-  const tempPath = req.file.path;
-  const targetPath = path.join(
-    __dirname,
-    `../../html/data/musicFiles/music${page}.mp3`
-  );
+//   const tempPath = req.file.path;
+//   const targetPath = path.join(
+//     __dirname,
+//     `../../html/data/musicFiles/music${page}.mp3`
+//   );
 
-  fs.rename(tempPath, targetPath, (err) => {
-    if (err) {
-      console.error("Error replacing music file:", err);
-      return res.status(500).send("Failed to replace the music file.");
-    }
-    res.send(`Music file for page ${page} replaced successfully!`);
-  });
-};
+//   fs.rename(tempPath, targetPath, (err) => {
+//     if (err) {
+//       console.error("Error replacing music file:", err);
+//       return res.status(500).send("Failed to replace the music file.");
+//     }
+//     res.send(`Music file for page ${page} replaced successfully!`);
+//   });
+// };
 
-module.exports = { uploadMusic };
+// module.exports = { uploadMusic };
