f90f0b0b4184f68adbc2c37a6dc0edc6bf7b506b
AYAZ MAHMOOD <hmayaz528@gmail.com>
2025-02-02 14:50:52 +0500

    initial commit

--- api/index.js
+++ api/index.js
@@ -71,4 +71,7 @@ app.get("/download-zip", async (req, res) => {
   }
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
