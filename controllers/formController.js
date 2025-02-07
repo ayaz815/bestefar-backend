ebc22303de34644f34fba83fbc93fe17eeaf02eb
AYAZ MAHMOOD <hmayaz528@gmail.com>
2025-02-07 10:29:18 +0500

    update file paths to aws

--- controllers/formController.js
+++ controllers/formController.js
@@ -28,8 +28,7 @@ const saveForm = async (req, res) => {
     //   __dirname,
     //   "../../html/data/content/content.json"
     // );
-    const jsonFilePath =
-      "/var/www/html/bestefar-html/data/content/content.json";
+    const jsonFilePath = "/var/www/bestefar-html/data/content/content.json";
 
     console.log("Resolved file path:", jsonFilePath);
 
