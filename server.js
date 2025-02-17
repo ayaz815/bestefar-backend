60f5cda445bf4826e8d38304aa43284dd884a871
AYAZ MAHMOOD <hmayaz528@gmail.com>
2025-02-17 11:30:07 +0500

    cors policy

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(
     origin: [
       "http://localhost:5173", // Allow local development
       "http://bestefar-frontend.s3-website.eu-north-1.amazonaws.com", // Allow S3 frontend
+      "https://bestefar.no",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
