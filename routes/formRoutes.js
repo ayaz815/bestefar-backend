04eba3d4d85852f394d82e041f07f721a4c3a3cb
Ayaz Mahmood <ayazmahmood@usama.m>
2025-07-26 09:23:08 +0500

    Update save-form route

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 // Use multer middleware for handling file uploads in `saveForm`
-router.post("/save-form", upload.single("musicFile"), saveForm);
-
+// router.post("/save-form", upload.single("musicFile"), saveForm);
+router.post("/save-form", upload.none(), saveForm);
 module.exports = router;
