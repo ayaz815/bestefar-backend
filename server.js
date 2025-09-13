0498efb8b6459118b50ca54efd281f8fc5ff671c
ayaz815 <hmayaz528@gmail.com>
2025-09-13 19:56:14 +0500

    Update server.json file

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
 app.use(compression());
+app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   "http://localhost:5173",
